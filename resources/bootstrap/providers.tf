30839b3a38e090d1cca9ccf27fe129ef1670649e
TheIronRock95 <58424695+TheIronRock95@users.noreply.github.com>
2025-04-13 19:54:55 +0200

    chore: revert to 5.0

--- resources/bootstrap/providers.tf
+++ resources/bootstrap/providers.tf
@@ -7,7 +7,7 @@ terraform {
   required_providers {
     talos = {
       source  = "siderolabs/talos"
-      version = "0.7.1"
+      version = "0.5.0"
     }
     proxmox = {
       source  = "bpg/proxmox"
